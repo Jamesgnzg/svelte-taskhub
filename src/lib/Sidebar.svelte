6df0f60d6ffb4653f8a29efb1728243918b33bfb
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-11-02 15:58:00 +0800

    add sidebar to layout

---
+++ src/lib/Sidebar.svelte
@@ -0,0 +1,8 @@
+<div class="w-full rounded-l-md bg-gray-50 p-5 opacity-75">
+	<ul role="list">
+		<li>Scout's Team</li>
+		<li>Scout's Team</li>
+		<li>Scout's Team</li>
+		<li>Scout's Team</li>
+	</ul>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/Header.svelte';
+	import Sidebar from '$lib/Sidebar.svelte';
 
 	let { children } = $props();
 </script>
@@ -10,7 +11,12 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<main>
+<div class="grid h-screen grid-rows-[auto_1fr_auto] bg-green-50">
 	<Header />
-	{@render children?.()}
-</main>
+	<div class="grid w-full grid-cols-2 pl-3 md:grid-cols-[auto_1fr]">
+		<Sidebar />
+		<main class="-ml-1 space-y-4 rounded-lg bg-white shadow-2xl">
+			{@render children?.()}
+		</main>
+	</div>
+</div>
