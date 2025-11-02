d46f0573c57ad2706971a95c1f8c1bbd93b4bf2b
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-11-02 17:32:20 +0800

    Add accordion

---
+++ src/lib/Accordion.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let open = false;
+	import { ChevronUp, ChevronDown } from '@lucide/svelte';
+	import { slide } from 'svelte/transition';
+
+	const handleClick = () => (open = !open);
+</script>
+
+<div class="hover:cursor-pointer">
+	<div class="flex w-full rounded-md p-2 hover:bg-green-100">
+		<div class="mr-1">
+			<slot name="name">
+				<p>Name</p>
+			</slot>
+		</div>
+		<button class="cursor-pointer" on:click={handleClick}>
+			{#if open}
+				<ChevronUp />
+			{:else}
+				<ChevronDown />
+			{/if}
+		</button>
+	</div>
+	{#if open}
+		<div class="mt-3 p-2" transition:slide>
+			<slot name="sublinks">
+				<p>No available sublinks</p>
+			</slot>
+		</div>
+	{/if}
+</div>
--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -1,8 +1,11 @@
-<div class="w-full rounded-l-md bg-gray-50 p-5 opacity-75">
+<script>
+	import Accordion from './Accordion.svelte';
+</script>
+
+<div class="w-3xs rounded-l-md bg-gray-50 p-5 opacity-75">
 	<ul role="list">
-		<li>Scout's Team</li>
-		<li>Scout's Team</li>
-		<li>Scout's Team</li>
-		<li>Scout's Team</li>
+		<Accordion>
+			<p class="text-sm font-semibold" slot="name">Favorites</p>
+		</Accordion>
 	</ul>
 </div>
