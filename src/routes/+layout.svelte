aa408ed0ef115f5d963cb4db52380d511493b38d
Jamesgnzg <j.jgonzaga03@gmail.com>
2025-11-05 19:11:22 +0800

    Add left space in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,12 +13,14 @@
 
 <div class="grid h-screen grid-rows-[auto_1fr_auto] bg-emerald-50">
 	<Header />
-	<div
-		class="mt-1 ml-3 grid w-full grid-cols-2 gap-0.5 rounded-lg bg-white md:grid-cols-[auto_1fr]"
-	>
-		<Sidebar />
-		<main class="space-y-4 rounded-lg bg-white shadow-[-5px_-5px_20px_0px_rgba(226,240,231,1)]">
-			{@render children?.()}
-		</main>
+	<div class="pl-4">
+		<div class="mt-1 grid w-full grid-cols-2 gap-0.5 rounded-lg bg-white md:grid-cols-[auto_1fr]">
+			<Sidebar />
+			<main
+				class="space-y-4 rounded-lg bg-white p-5 shadow-[-5px_-5px_20px_0px_rgba(226,240,231,1)]"
+			>
+				{@render children?.()}
+			</main>
+		</div>
 	</div>
 </div>
